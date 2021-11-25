c4200b5239684a4cbf82fdfbf91fbb96a30f5543
Foxes-one <foxes.one@mail.ru>
2021-11-25 16:59:36 +0700

    init

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,16 +3,17 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 //import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 //public class MinMaxClass {
-    public int Min(int a, int b){
+    public static int Min(int a, int b){
         if(a<b){
             return a;
         }
         else return b;
     }
 
-    public int Max(int a, int b){
+    public static int Max(int a, int b){
         if(a<b){
             return b;
         }
--- app/src/test/java/com/example/lab1/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab1/ExampleUnitTest.java
@@ -3,12 +3,12 @@ package com.example.lab1;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
+/*
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         MainActivity t = new MainActivity();
-
+       // @Test
         assertEquals(t.Min(4, 30), 4);
         assertEquals(t.Min(31, 3), 3);
         assertEquals(t.Min(50, 50), 50);
@@ -19,4 +19,22 @@ public class ExampleUnitTest {
         assertEquals(t.Max(50, 50), 50);
         assertEquals(t.Max(-4, 4), 4);
     }
+}*/
+public class ExampleUnitTest {
+    @Test
+    public void Min1() { assertEquals(MainActivity.Min(4,30),4); }
+    @Test
+    public void Min2() { assertEquals(MainActivity.Min(31,3),3); }
+    @Test
+    public void Min3() { assertEquals(MainActivity.Min(4,4),4); }
+    @Test
+    public void Min4() { assertEquals(MainActivity.Min(-4,4),-4); }
+    @Test
+    public void Max1() { assertEquals(MainActivity.Max(4,30),30); }
+    @Test
+    public void Max2() { assertEquals(MainActivity.Max(31,3),31); }
+    @Test
+    public void Max3() { assertEquals(MainActivity.Max(4,4),4); }
+    @Test
+    public void Max4() { assertEquals(MainActivity.Max(-4,4),4); }
 }
